b8689c04cbd84be6542cc664207d0ca3cb3d0928
GGCodeLatam <gg.code.latam@gmail.com>
2022-09-04 19:50:20 -0300

    mas cambios

--- components/RandomMovie/index.jsx
+++ components/RandomMovie/index.jsx
@@ -1,27 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useState, useEffect, useRef } from 'react'
-import tmdbApi, { category, movieType , options} from '../../api/tmdbApi';
+import tmdbApi, { category, movieType, options } from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
 import { useMediaQuery } from 'react-responsive'
 import random from 'random'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Image from 'next/image';
 
 const NowPlaying = () => {
 
-
-    const [randomMovie, setRandomMovie] = useState(1);
+    const [randomMovie, setRandomMovie] = useState(9);
 
     useEffect(() => {
-        const randomMovie = async () => {
-            let min;
-            let max;
-            random.int((min = 10), (max = 20))
-        }
-        setRandomMovie
-    },[])
-
-
-
+        setTimeout(() => {
+            const aleatorio = async () => {
+                let min;
+                let max;
+                try {
+                    const response = random.int((min = 10), (max = 20));
+                    setRandomMovie(response);
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+        }, 1000);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     const isDesktopOrLaptop = useMediaQuery({
         query: '(min-width: 1224px)'
@@ -50,14 +53,14 @@ const NowPlaying = () => {
             }
         }
         getMovies();
-           // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div className='m-2 mr lg:mr-20'>
             <div className='content-center row-span-5'>
-                <div className="grid grid-cols-1 gap-3">
-                    <img src={apiConfig.originalImage(movieItems.backdrop_path)} alt="" className='w-full h-72 rounded-3xl' />
+                <div className="grid grid-cols-1 gap-3 relative">
+                    <img src={apiConfig.w500Image(movieItems.backdrop_path)} alt="" className='w-full h-72 rounded-3xl' />
                 </div>
             </div>
         </div>
