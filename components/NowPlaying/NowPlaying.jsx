86896e3b22b7e86a10cf6277236d45d1142f07fe
GGCodeLatam <gg.code.latam@gmail.com>
2022-09-05 14:18:16 -0300

    probando

--- components/NowPlaying/NowPlaying.jsx
+++ components/NowPlaying/NowPlaying.jsx
@@ -25,14 +25,12 @@ const NowPlaying = () => {
                     setMovieItems(response.results.slice(0, 1));
                     console.log(response.results.slice(0, 2));
                 }
-
             } catch (err) {
                 console.log(err);
             }
         }
         getMovies();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [movieItems]);
+    }, [isDesktopOrLaptop]);
 
     return (
         <div className='m lg:m-2 mr-0 lg:mr-14'>
@@ -45,8 +43,8 @@ const NowPlaying = () => {
                         movieItems.map((item, i) => (
                             <div key={i} className="col-span-1">
                                 <article className="m-5 shadow-xl rounded-3xl bg-cover bg-center w-auto h-20 lg:h-48 transform duration-500 hover:-translate-y-5 cursor-pointer group" style={{ backgroundImage: `url(${apiConfig.w500Image(item.backdrop_path)})` }}>
-                                    <div className="bg-black rounded-3xl bg-opacity-20 w-full h-20 lg:h-48 px-10 flex flex-wrap flex-row pt-18 hover:bg-opacity-75 transform duration-300">
-                                        <h1 className="text-white text-xs lg:text-xl transform translate-y-5 group-hover:translate-y-0 duration-300">
+                                    <div className="bg-black rounded-3xl bg-opacity-20 w-full h-20 lg:h-48 px-2 lg:px-5 flex flex-wrap flex-row  pt-22  lg:pt-18 hover:bg-opacity-75 transform duration-300">
+                                        <h1 className="text-white text-xs lg:text-xl transform translate-y-5 group-hover:translate-y-0 duration-300 drop-shadow-lg shadow-black">
                                             {item.title}
                                         </h1>
                                         <div className="w-auto h-auto bg-yellow-500 rounded-full mb-2 transform translate-y-5 group-hover:translate-y-0 duration-300">
